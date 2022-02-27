e9ff14702d307cbfae275b73768757d442fccd10
AugustineKadima <kadimasam@gmail.com>
2022-02-27 14:57:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    
+    return record.find((item)=>{
+        if(item.result === "W"){
+            return item = item["year"]
+        }
+    })
+}
